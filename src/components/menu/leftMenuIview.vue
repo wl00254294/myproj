299dd92cf110521eb41779afd16cf5529ab25ff2
EricWu <43841531+wl00254294@users.noreply.github.com>
2021-10-23 21:01:57 +0800

    Update leftMenuIview.vue
    dd

--- src/components/menu/leftMenuIview.vue
+++ src/components/menu/leftMenuIview.vue
@@ -45,7 +45,7 @@ export default {
   data() {
     return {
         items : [{
-            title : '功能區sdsdf',
+            title : '功能區',
             navs: [...menu]
         }],
         themetype: "light"
